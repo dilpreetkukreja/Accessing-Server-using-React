bead731a088d0604b4c020b3b54117d1545f7b5f
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-11-22 17:54:27 -0800

    Updates

---
+++ precache-manifest.b7aa295d0826aed27ccdac9363e28fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18161bdfd13383c8dd110dd9ab39859",
+    "url": "/posts-App/index.html"
+  },
+  {
+    "revision": "8a20b1f8b20a8660a17c",
+    "url": "/posts-App/static/css/main.c6ee77bd.chunk.css"
+  },
+  {
+    "revision": "a2c51ecc38fa18bd985a",
+    "url": "/posts-App/static/js/2.c37371d8.chunk.js"
+  },
+  {
+    "revision": "8a20b1f8b20a8660a17c",
+    "url": "/posts-App/static/js/main.76c4498d.chunk.js"
+  },
+  {
+    "revision": "1f3ebb2c6b9211d8d50a",
+    "url": "/posts-App/static/js/runtime-main.c28d04d2.js"
+  }
+]);
