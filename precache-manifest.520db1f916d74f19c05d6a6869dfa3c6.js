2688091bb00d91ba16ffe78e51df8f9a7f378221
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-11-22 17:56:44 -0800

    Updates

---
+++ precache-manifest.520db1f916d74f19c05d6a6869dfa3c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c32d36a50b2e6cd85b440cd83c9e2ab",
+    "url": "/Accessing-Server-using-React/index.html"
+  },
+  {
+    "revision": "c70e416e51ae9fb2b057",
+    "url": "/Accessing-Server-using-React/static/css/main.c6ee77bd.chunk.css"
+  },
+  {
+    "revision": "a2c51ecc38fa18bd985a",
+    "url": "/Accessing-Server-using-React/static/js/2.c37371d8.chunk.js"
+  },
+  {
+    "revision": "c70e416e51ae9fb2b057",
+    "url": "/Accessing-Server-using-React/static/js/main.ee2e7aef.chunk.js"
+  },
+  {
+    "revision": "8e15cf6356e66b2afde2",
+    "url": "/Accessing-Server-using-React/static/js/runtime-main.6bd84102.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/posts-App/precache-manifest.b7aa295d0826aed27ccdac9363e28fbb.js"
+  "/Accessing-Server-using-React/precache-manifest.520db1f916d74f19c05d6a6869dfa3c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/posts-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Accessing-Server-using-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
